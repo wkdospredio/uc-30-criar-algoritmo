25be46fba81e08259d6b5ff06a949342548a89b5
wkdospredio <wevertonkaua609@gmail.com>
2026-03-03 14:39:10 -0300

    Add files via upload

---
+++ UC-algoritmo(03-03-26)/repetição.py
@@ -0,0 +1,19 @@
+print("Vou ser rico")
+print("Vou ser rico")
+print("Vou ser rico")
+print("Vou ser rico")
+cont = 1
+
+while cont <=10:
+    print("Vou ser rico")
+    cont += 1 # cont = cont + 1
+
+print("FIM")
+
+print(list(range(11)))
+print(list(range(1,15,3)))
+
+soma = 0
+for termo in range(1,11):
+    soma += termo
+print(soma)
---
+++ UC-algoritmo(03-03-26)/teste.py
@@ -0,0 +1,2 @@
+numero1 = input("Digite o primeiro numero")
+numero2 = input("Digite o segundo numero")
